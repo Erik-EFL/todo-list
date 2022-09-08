8169594a3dd176d066389c629e2337928fcf0116
erik lima <erik.lima.f@outlook.com>
2022-09-07 21:28:03 -0300

    Adicionando ultimos requisitos

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 const button = document.querySelector('#criar-tarefa');
 const input = document.querySelector('#texto-tarefa');
 const limpar = document.querySelector('#apaga-tudo');
+const saveTodosButton = document.querySelector('#salvar-tarefas');
+const removeSelected = document.querySelector('#remover-selecionado');
+const moveUp = document.querySelector('#mover-cima');
+const moveDown = document.querySelector('#mover-baixo');
 const clearFinish = document.querySelector('#remover-finalizados');
+
+function list() {
+  return document.querySelector('#lista-tarefas');
+}
+
+function selected() {
+  return document.querySelector('li.selected');
+}
+
+function saveTodos() {
+  const save = list();
+  localStorage.setItem('list', save.innerHTML);
+}
 
 function mudarCor(event) {
   const color = document.querySelector('.selected');
@@ -16,7 +33,6 @@ function lineThrough(event) {
 }
 
 function criar() {
-  const listAdd = document.querySelector('#lista-tarefas');
   const item = document.createElement('li');
   item.addEventListener('click', mudarCor);
   item.addEventListener('dblclick', lineThrough);
@@ -25,15 +41,17 @@ function criar() {
     alert('Please enter a value');
   } else {
     item.innerHTML = input.value;
-    listAdd.appendChild(item);
+    const newItem = list();
+    newItem.appendChild(item);
   }
   input.value = '';
 }
 
 function limparLista() {
-  const list = document.querySelectorAll('.item');
-  for (let i = 0; i < list.length; i += 1) {
-    list[i].parentElement.removeChild(list[i]);
+  const listAll = document.querySelectorAll('.item');
+  for (let i = 0; i < listAll.length; i += 1) {
+    listAll[i].parentElement.removeChild(listAll[i]);
+    saveTodos();
   }
 }
 
@@ -41,9 +59,66 @@ function FinishClear() {
   const completed = document.querySelectorAll('.completed');
   for (let i = 0; i < completed.length; i += 1) {
     completed[i].parentElement.removeChild(completed[i]);
+    saveTodos();
   }
 }
+
+/* Bonus */
+
+function addSavedTodos() {
+  const AddList = list();
+  AddList.innerHTML = localStorage.getItem('list');
+  const items = document.querySelectorAll('.item');
+  for (let i = 0; i < items.length; i += 1) {
+    items[i].addEventListener('click', mudarCor);
+    items[i].addEventListener('dblclick', lineThrough);
+  }
+}
+
+function moveUpItem() {
+  const selectedUp = selected();
+  if (!selectedUp) return null;
+  const previousItem = selectedUp.previousElementSibling;
+  if (previousItem) {
+    selectedUp.parentElement.insertBefore(selectedUp, previousItem);
+    saveTodos();
+  }
+}
+
+function moveDownItem() {
+  const selectedDown = selected();
+  if (!selectedDown) return null;
+  const nextItem = selectedDown.nextElementSibling;
+  if (nextItem) {
+    selectedDown.parentElement.insertBefore(nextItem, selectedDown);
+    saveTodos();
+  }
+}
+
+function removeSelectedItem() {
+  const removeSelection = selected();
+  removeSelection.remove();
+  saveTodos();
+}
+
+/* Click */
 
 limpar.addEventListener('click', limparLista);
 clearFinish.addEventListener('click', FinishClear);
 button.addEventListener('click', criar);
+saveTodosButton.addEventListener('click', saveTodos);
+removeSelected.addEventListener('click', removeSelectedItem);
+moveUp.addEventListener('click', moveUpItem);
+moveDown.addEventListener('click', moveDownItem);
+
+/* Buttons press */
+
+input.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    criar();
+  }
+});
+
+window.onload = () => {
+  addSavedTodos();
+};
